cb3dafdd3696640d38443ac35f45760f4934cb86
Kovak823 <71556738+Kovak823@users.noreply.github.com>
2020-11-04 20:21:14 +0100

    Add files via upload

---
+++ calculator08buggy.cpp
@@ -0,0 +1,299 @@
+
+/*
+	calculator08buggy.cpp
+
+	Helpful comments removed.
+
+	We have inserted 3 bugs that the compiler will catch and 3 that it won't.
+*/
+
+#include "std_lib_facilities.h"
+
+const char let = '#';
+const char quit = 'Q';
+const char print = ';';
+const char number = '8';
+const char name = 'a';
+const char square_r = '@';
+const string quitk = "exit";
+const string sqrtk = "sqrt";
+const string prompt = "> ";
+const string result = "= ";
+
+double expression();
+double primary();
+
+struct Token 
+{
+public:
+	char kind;
+	double value;
+	string name;
+	Token(char ch) :kind(ch), value(0) { }
+	Token(char ch, double val) :kind(ch), value(val) { }
+	Token(char ch, string s) :kind(ch), name(s) {}
+};
+
+class Token_stream 
+{
+public:
+	Token_stream() :full(0), buffer(0) { }
+	Token get();
+	void putback(Token t) { buffer = t; full = true; }
+	void ignore(char c);
+private:
+	bool full;
+	Token buffer;
+};
+
+class Variable 
+{
+public:
+	string name;
+	double value;
+	Variable(string n, double v) :name(n), value(v) { }
+};
+
+vector<Variable> names;
+
+double get_value(string s)
+{
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return names[i].value;
+	error("get: undefined name ", s);
+	return 0;
+}
+
+void set_value(string s, double d)
+{
+	for (int i = 0; i <= names.size(); ++i)
+		if (names[i].name == s) {
+			names[i].value = d;
+			return;
+		}
+	error("set: undefined name ", s);
+}
+
+bool is_declared(string s)
+{
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return true;
+	return false;
+}
+
+double define_name(string var, double val){
+	if(is_declared(var)) error (var, "declared twice");
+	names.push_back(Variable{var,val});
+	return val;
+}
+
+Token Token_stream::get()
+{
+	if (full) { full = false; return buffer; }
+	char ch;
+	cin >> ch;
+	switch (ch) {
+	case '(':
+	case ')':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case print:
+	case quit:
+	case '=':
+	case let:
+		return Token(ch);
+	case '.':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	{	
+	cin.putback(ch);
+	double val;
+	cin >> val;
+	return Token(number, val);
+	}
+	default:
+		if (isalpha(ch)) {
+			string s;
+			s += ch;
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+			cin.putback(ch);
+			if (s == sqrtk) return Token{square_r};
+			else if (s == quitk) return Token{quit};
+			else if(is_declared(s))
+				return Token(number, get_value(s));
+			return Token{name, s};
+		}
+		error("Bad token");
+	}
+	return 0;
+}
+
+void Token_stream::ignore(char c)
+{
+	if (full && c == buffer.kind) {
+		full = false;
+		return;
+	}
+	full = false;
+
+	char ch;
+	while (cin >> ch)
+		if (ch == c) return;
+}
+
+Token_stream ts;
+
+double expression();
+
+double calc_sqrt(){
+	char ch;
+	if(cin.get(ch) && ch != '(') error ("'(' expected");
+	cin.putback(ch);
+	double d = expression();
+	return sqrt(d);
+}
+
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case '(':
+	{	
+		double d = expression();
+		t = ts.get();
+		if (t.kind != ')') error("'(' expected");
+	return d;
+	}
+		case '-':
+			return -primary();
+		case number:
+			return t.value;
+		case name:
+			return get_value(t.name);
+		case square_r:
+			return calc_sqrt();
+		default:
+			error("primary expected");
+			return 0;
+	}
+}
+
+double term()
+{
+	double left = primary();
+	while (true) {
+		Token t = ts.get();
+		switch (t.kind) {
+		case '*':
+			left *= primary();
+			break;
+		case '/':
+		{	double d = primary();
+		if (d == 0) error("divide by zero");
+		left /= d;
+		break;
+		}
+		default:
+			ts.putback(t);
+			return left;
+		}
+	}
+}
+
+double expression()
+{
+	double left = term();
+	while (true) {
+		Token t = ts.get();
+		switch (t.kind) {
+		case '+':
+			left += term();
+			break;
+		case '-':
+			left -= term();
+			break;
+		default:
+			ts.putback(t);
+			return left;
+		}
+	}
+}
+
+double declaration()
+{
+	Token t = ts.get();
+	if (t.kind != 'a') error("name expected in declaration");
+	string name = t.name;
+	if (is_declared(name)) error(name, " declared twice");
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of ", name);
+	double d = expression();
+	names.push_back(Variable(name, d));
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.putback(t);
+		return expression();
+	}
+}
+
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
+
+
+
+void calculate()
+{
+	while (true) try {
+		cout << prompt;
+		Token t = ts.get();
+		while (t.kind == print) t = ts.get();
+		if (t.kind == quit) return;
+		ts.putback(t);
+		cout << result << statement() << endl;
+	}
+	catch (runtime_error& e) {
+		cerr << e.what() << endl;
+		clean_up_mess();
+	}
+}
+
+int main()
+
+try {
+	define_name("k", 1000);
+	calculate();
+	return 0;
+}
+catch (exception& e) {
+	cerr << "exception: " << e.what() << endl;
+	char c;
+	while (cin >> c && c != ';');
+	return 1;
+}
+catch (...) {
+	cerr << "exception\n";
+	char c;
+	while (cin >> c && c != ';');
+	return 2;
+}
